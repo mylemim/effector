adbbc46b435bf4ddcd690a12e7ac1b03c0204db8
Mario Milas <mylemim@gmail.com>
2015-07-06 19:53:39 +0200

    Added projectile firing behavior

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float LifeTimeSeconds = 2;
+	private float timeOfCreation;
+
+	void Start()
+	{
+		timeOfCreation = Time.time;
+	}
+
+	void Update ()
+	{
+		if(Time.time - timeOfCreation> LifeTimeSeconds)
+			Destroy (gameObject);
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/ShipFiring.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipFiring : MonoBehaviour
+{
+
+	public GameObject ProjectilePrefab;
+	[Range(0,1)]
+	public float RelativeShootingDistanceFromShip = 1;
+	public float ShotsPerSecond = 20f;
+	public float ProjectileSpeed = 10f;
+
+	private float lastTimeOfFiring;
+	private float delayBetweenShots {
+		get{
+			if(ShotsPerSecond >0)
+				return 1/ShotsPerSecond;
+			return 0;
+		}
+	}
+
+	private Rigidbody2D shipRigidbody;
+
+	void Start ()
+	{
+		shipRigidbody = gameObject.GetComponent<Rigidbody2D> ();
+		lastTimeOfFiring = Time.time;
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButton(0) && Time.time-lastTimeOfFiring>delayBetweenShots)
+			Shoot ();
+	}
+
+	void Shoot()
+	{
+		Vector2 mousePosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+		Vector2 direction = Camera.main.ScreenToWorldPoint (mousePosition) - transform.position;
+		direction.Normalize ();
+		
+		GameObject spawnedProjectile = (GameObject)Instantiate (ProjectilePrefab, 
+		                                                        (Vector2)transform.position+direction*RelativeShootingDistanceFromShip, 
+		                                                        transform.rotation);
+		Rigidbody2D projectileRigidbody = spawnedProjectile.GetComponent<Rigidbody2D> ();
+		
+		projectileRigidbody.AddForce (direction * ProjectileSpeed);
+		lastTimeOfFiring = Time.time;
+	}
+}
