52e633d8a2efcf6e9586d784ffa3ec64820007f6
Mario Milas <mylemim@gmail.com>
2015-07-07 22:05:41 +0200

    Added spawn point logic

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour
+{
+
+	public GameObject gameObjectToInstantiate;
+	public float waitingTimeBetweenSpawns = 5; //seconds
+	public int maxGameObjectsToInstantiate = 10;
+	private int instantiatedGameObjects;
+	private float lastSpawnTime;
+
+	// Use this for initialization
+	void Start ()
+	{
+		instantiatedGameObjects = 0;
+		lastSpawnTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (instantiatedGameObjects < maxGameObjectsToInstantiate && Time.time - lastSpawnTime > waitingTimeBetweenSpawns) {
+			GameObject spawnedGameObject = (GameObject) Instantiate(gameObjectToInstantiate, gameObject.transform.position, Quaternion.identity);
+			spawnedGameObject.AddComponent<SpawnedObject>();
+			SpawnedObject spawnedObjectBehavior = spawnedGameObject.GetComponent<SpawnedObject>();
+			spawnedObjectBehavior.spawnPoint= this;
+
+			lastSpawnTime = Time.time;
+			instantiatedGameObjects += 1;
+		}
+	}
+
+	public void NotifyOfSpawnedObjectDestroyed()
+	{
+		instantiatedGameObjects -= 1;
+		instantiatedGameObjects = Mathf.Max (instantiatedGameObjects, 0);
+	}
+}
---
+++ Assets/Scripts/SpawnedObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnedObject : MonoBehaviour {
+
+	public SpawnPoint spawnPoint;
+
+	void OnDestroy ()
+	{
+		if (spawnPoint != null) {
+			spawnPoint.NotifyOfSpawnedObjectDestroyed();
+		}
+	}
+}
