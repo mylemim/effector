0ba07f958e6528fc7b4c6e9d6dca6674fe091a7e
Mario Milas <mylemim@gmail.com>
2015-07-07 22:32:56 +0200

    Added layering so enemies can pass through the world bounds

---
+++ Assets/Scripts/WorldBounds.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldBounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Set collisions so the enemies can enter and exit the arena at will
+		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Enemies"), LayerMask.NameToLayer("World Bounds"));
+	}
+}
