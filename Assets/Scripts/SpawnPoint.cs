8174e054f20ff6e75060a2bb6a214933740b1b4a
Mario Milas <mylemim@gmail.com>
2016-04-11 23:11:26 +0200

    Added group object reference to spawn point

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -4,36 +4,46 @@ using System.Collections;
 public class SpawnPoint : MonoBehaviour
 {
 
-	public GameObject gameObjectToInstantiate;
-	public float waitingTimeBetweenSpawns = 5; //seconds
-	public int maxGameObjectsToInstantiate = 10;
-	private int instantiatedGameObjects;
-	private float lastSpawnTime;
+    public GameObject gameObjectToInstantiate;
+    public float waitingTimeBetweenSpawns = 5; //seconds
+    public int maxGameObjectsToInstantiate = 10;
+    private int instantiatedGameObjects;
+    private float lastSpawnTime;
 
-	// Use this for initialization
-	void Start ()
-	{
-		instantiatedGameObjects = 0;
-		lastSpawnTime = 0;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (instantiatedGameObjects < maxGameObjectsToInstantiate && Time.time - lastSpawnTime > waitingTimeBetweenSpawns) {
-			GameObject spawnedGameObject = (GameObject) Instantiate(gameObjectToInstantiate, gameObject.transform.position, Quaternion.identity);
-			spawnedGameObject.AddComponent<SpawnedObject>();
-			SpawnedObject spawnedObjectBehavior = spawnedGameObject.GetComponent<SpawnedObject>();
-			spawnedObjectBehavior.spawnPoint= this;
+    //Name of the group object the spawned objects will be stored in
+    public GameObject targetGroup;
 
-			lastSpawnTime = Time.time;
-			instantiatedGameObjects += 1;
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        instantiatedGameObjects = 0;
+        lastSpawnTime = 0;
+    }
 
-	public void NotifyOfSpawnedObjectDestroyed()
-	{
-		instantiatedGameObjects -= 1;
-		instantiatedGameObjects = Mathf.Max (instantiatedGameObjects, 0);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (instantiatedGameObjects < maxGameObjectsToInstantiate && Time.time - lastSpawnTime > waitingTimeBetweenSpawns)
+        {
+            GameObject spawnedGameObject = (GameObject)Instantiate(gameObjectToInstantiate, gameObject.transform.position, Quaternion.identity);
+
+            //Reference the target group
+            if (targetGroup)
+                spawnedGameObject.transform.parent = targetGroup.transform;
+
+            //Add reference to spawn so we can recognize when this object has been destroyed
+            spawnedGameObject.AddComponent<SpawnedObject>();
+            SpawnedObject spawnedObjectBehavior = spawnedGameObject.GetComponent<SpawnedObject>();
+            spawnedObjectBehavior.spawnPoint = this;
+
+            lastSpawnTime = Time.time;
+            instantiatedGameObjects += 1;
+        }
+    }
+
+    public void NotifyOfSpawnedObjectDestroyed()
+    {
+        instantiatedGameObjects -= 1;
+        instantiatedGameObjects = Mathf.Max(instantiatedGameObjects, 0);
+    }
 }
