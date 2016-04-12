035c1a3b9aca4add4cde4663d0e742145ddcd524
Mario Milas <mylemim@gmail.com>
2016-04-12 21:42:37 +0200

    Added static creation to randomized environment without colliding world objects

--- Assets/Scripts/InitializeStaticEnvironment.cs
+++ Assets/Scripts/InitializeStaticEnvironment.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class InitializeStaticEnvironment : MonoBehaviour
 {
-
     public GameObject gameObjectToInstantiate;
     public int maxGameObjectsToInstantiate = 10;
+
+    public string staticLayerName = "World";
 
     public float minX = 0;
     public float maxX = 10;
 
     public float minY = 0;
     public float maxY = 10;
+
+    public float maxDistanceRadius = 3;
 
     //Name of the group object the spawned objects will be stored in
     public GameObject targetGroup;
@@ -20,10 +23,16 @@ public class InitializeStaticEnvironment : MonoBehaviour
     void Start()
     {
         int targetNumberOfObjects = Random.Range(2, maxGameObjectsToInstantiate);
-
         for (int i = 0; i < targetNumberOfObjects; i++)
         {
-            Vector2 newPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+            Vector2 newPosition;
+            bool isNotIntersectingOtherStaticObjects;
+            do
+            {
+                newPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                isNotIntersectingOtherStaticObjects = Physics2D.OverlapCircle(newPosition, maxDistanceRadius, 1 << LayerMask.NameToLayer(staticLayerName)) != null;
+            } while (isNotIntersectingOtherStaticObjects);
+
             GameObject spawnedGameObject = (GameObject)Instantiate(gameObjectToInstantiate, newPosition, Quaternion.identity);
             if (targetGroup)
                 spawnedGameObject.transform.parent = targetGroup.transform;
