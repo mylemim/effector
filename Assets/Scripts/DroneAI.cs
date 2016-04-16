220071fc15003af22429ff12a014984d7a5ae249
Mario Milas <mylemim@gmail.com>
2016-04-17 00:00:56 +0200

    Added avoidance of world objects to enemies

--- Assets/Scripts/DroneAI.cs
+++ Assets/Scripts/DroneAI.cs
@@ -1,29 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DroneAI : MonoBehaviour {
+public class DroneAI : MonoBehaviour
+{
 
-	public string TargetTag = "Player";
-	public float Speed = 4f;
+    public string TargetTag = "Player";
+    public float Speed = 4f;
 
-	private GameObject targetGameObject;
+    /// <summary>
+    /// Distance from other objects before the script-owning objects starts to avoid them
+    /// </summary>
+    private float objectAvoidanceDistance = 5f;
 
-	private Rigidbody2D droneRigidbody;
+    /// <summary>
+    /// Angle of turning when an obstructing object is detected
+    /// </summary>
+    private float turnSpeed = 8;
 
-	// Use this for initialization
-	void Start () {
-		targetGameObject = GameObject.FindGameObjectWithTag (TargetTag);
-		droneRigidbody = gameObject.GetComponent<Rigidbody2D> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector2 direction = targetGameObject.transform.position - transform.position;
-		direction.Normalize ();
+    /// <summary>
+    /// We're giving a randomized turn direction (either up (1) or down (-1))
+    /// </summary>
+    private int preferredTurnDirection = Random.Range(1, 2) % 2 == 0 ? 1 : -1;
 
-		float rotationAmount = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler(0f, 0f, rotationAmount - 90);
+    private GameObject targetGameObject;
+    private Rigidbody2D droneRigidbody;
 
-		droneRigidbody.AddForce (direction * Speed);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        targetGameObject = GameObject.FindGameObjectWithTag(TargetTag);
+        droneRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 directionTowardsPlayer = targetGameObject.transform.position - transform.position;
+        directionTowardsPlayer.Normalize();
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, directionTowardsPlayer, objectAvoidanceDistance, LayerMask.GetMask(new string[] { "World" }));
+        if (hit.collider != null)
+            //Turn harder the closer you are to the target object
+            droneRigidbody.AddForce(Vector2.up * preferredTurnDirection * turnSpeed * hit.distance / objectAvoidanceDistance);
+
+        MoveInDirection(directionTowardsPlayer);
+    }
+
+    private void MoveInDirection(Vector2 direction)
+    {
+        float rotationAmount = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationAmount - 90);
+
+        droneRigidbody.AddForce(direction * Speed);
+    }
 }
