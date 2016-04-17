ea72e10aeae2e88e15ac23267f717abb14b34385
Mario Milas <mylemim@gmail.com>
2016-04-17 17:55:42 +0200

    Substituted spawn points logic for the spawn manager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject objectPrefab;
+    public float waitingTimeBetweenSpawns = 5; //seconds
+    public int gameObjectsToInstantiate = 10;
+    /// <summary>
+    /// Name of the group object the spawned objects will be stored in
+    /// </summary>
+    public GameObject targetGroup;
+    public Vector2[] spawnLocations;
+
+    private int instantiatedGameObjects;
+    private float lastSpawnTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        instantiatedGameObjects = 0;
+        lastSpawnTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnLocations.Length>0 && instantiatedGameObjects < gameObjectsToInstantiate && Time.time - lastSpawnTime > waitingTimeBetweenSpawns)
+        {
+            Vector2 spawnLocation = spawnLocations[Random.Range(0, spawnLocations.Length)];
+            GameObject spawnedGameObject = (GameObject)Instantiate(objectPrefab, spawnLocation, Quaternion.identity);
+
+            //Reference the target group
+            if (targetGroup)
+                spawnedGameObject.transform.parent = targetGroup.transform;
+
+            lastSpawnTime = Time.time;
+            instantiatedGameObjects += 1;
+        }
+    }
+
+    [ExecuteInEditMode]
+    void OnValidate()
+    {
+        gameObjectsToInstantiate = Mathf.Abs(gameObjectsToInstantiate);
+        waitingTimeBetweenSpawns = Mathf.Abs(waitingTimeBetweenSpawns);
+    }
+}
