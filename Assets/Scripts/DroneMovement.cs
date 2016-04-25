0fc1083b8fa528e1c85bd752fbe40f4bb1e4b1fe
Mario Milas <mylemim@gmail.com>
2016-04-25 23:55:08 +0200

    Added shooting drones

--- Assets/Scripts/DroneMovement.cs
+++ Assets/Scripts/DroneMovement.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DroneAI : MonoBehaviour
+public class DroneMovement : MonoBehaviour
 {
 
     public string TargetTag = "Player";
---
+++ Assets/Scripts/DroneShooting.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneShooting : MonoBehaviour
+{
+    public string TargetTag = "Player";
+    private GameObject targetGameObject;
+
+    public GameObject ProjectilePrefab;
+    [Range(0, 1)]
+    public float RelativeShootingDistanceFromShip = 1;
+    public float ShotsPerSecond = 20f;
+    public float ProjectileSpeed = 10f;
+
+    private float lastTimeOfFiring;
+    private float delayBetweenShots
+    {
+        get
+        {
+            if (ShotsPerSecond > 0)
+                return 1 / ShotsPerSecond;
+            return 0;
+        }
+    }
+
+    void Start()
+    {
+        lastTimeOfFiring = Time.time;
+        targetGameObject = GameObject.FindGameObjectWithTag(TargetTag);
+    }
+
+    void Update()
+    {
+        if (Time.time - lastTimeOfFiring > delayBetweenShots && targetGameObject!=null)
+            Shoot();
+    }
+
+    void Shoot()
+    {
+        Vector2 direction = targetGameObject.transform.position - transform.position;
+        direction.Normalize();
+
+        GameObject spawnedProjectile = (GameObject)Instantiate(ProjectilePrefab,
+                                                                (Vector2)transform.position + direction * RelativeShootingDistanceFromShip,
+                                                                transform.rotation);
+        Rigidbody2D projectileRigidbody = spawnedProjectile.GetComponent<Rigidbody2D>();
+
+        projectileRigidbody.AddForce(direction * ProjectileSpeed);
+        lastTimeOfFiring = Time.time;
+    }
+}
