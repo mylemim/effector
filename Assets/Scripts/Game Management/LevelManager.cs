2c0f32de05722068bf982724e66fd93fd78b46e9
Mario Milas <mylemim@gmail.com>
2016-09-05 23:20:36 +0200

    Added increasing of enemy count after each level

--- Assets/Scripts/Game Management/LevelManager.cs
+++ Assets/Scripts/Game Management/LevelManager.cs
@@ -5,14 +5,23 @@ public class LevelManager : MonoBehaviour {
 
 	private UiManager uiManager;
 	private SpawnManager spawnManager;
+	private InitializeStaticEnvironment environmentManager;
+
+	public int EnemiesPerLevelMultiplier = 2; 
 
 	//Awake is always called before any Start functions
 	void Awake()
 	{
 		uiManager = GameObject.Find ("UiManager").GetComponent<UiManager> ();
-		spawnManager =  GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
-	}
 
+		spawnManager =  GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+		spawnManager.GameObjectsToInstantiate = GameManager.instance.Level * EnemiesPerLevelMultiplier;
+	
+		environmentManager =  GameObject.Find("EnvironmentManager").GetComponent<InitializeStaticEnvironment>();
+		environmentManager.minGameObjectsToInstantiate = Mathf.RoundToInt( GameManager.instance.Level / 2f); 
+		environmentManager.maxGameObjectsToInstantiate = Mathf.RoundToInt( GameManager.instance.Level / 1.2f); 
+	}
+		
 	void Start(){
 		Debug.Log ("Current level: " + GameManager.instance.Level);
 	}
