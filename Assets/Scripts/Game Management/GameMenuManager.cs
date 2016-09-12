817a338f2c9874b8052d48f73bc7e6f6b5c43d4e
Mario Milas <mylemim@gmail.com>
2016-09-12 23:48:23 +0200

    Added a pause menu to the play level

---
+++ Assets/Scripts/Game Management/GameMenuManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenuManager : MenuManager {
+
+	const string ESCAPE = "escape";
+
+	public GameObject menu;
+
+	private bool isPaused = false;
+
+	void Start(){
+		Time.timeScale = 1;
+		menu.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (ESCAPE)) {
+			if (!isPaused)
+				this.Pause ();
+			else 
+				this.Resume ();
+		}
+	}
+
+	public void Pause(){
+		Time.timeScale = 0;
+		menu.SetActive (true);
+		isPaused = true;
+	}
+
+	public void Resume(){
+		Time.timeScale = 1;
+		menu.SetActive (false);
+		isPaused = false;
+	}
+}
