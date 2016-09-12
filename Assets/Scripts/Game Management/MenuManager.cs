30642d80d0830155495f3fd1826bccacdb3815eb
Mario Milas <mylemim@gmail.com>
2016-09-12 23:24:05 +0200

    Added credits

--- Assets/Scripts/Game Management/MenuManager.cs
+++ Assets/Scripts/Game Management/MenuManager.cs
@@ -3,9 +3,10 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
-{
-
+{	
+	const string MENU_LEVEL_NAME = "menu";
 	const string FIRST_LEVEL_NAME = "level";
+	const string CREDITS_LEVEL_NAME = "credits";
 
 	public void ExitGame ()
 	{
@@ -15,5 +16,13 @@ public class MenuManager : MonoBehaviour
 	public void StartNewGame ()
 	{
 		SceneManager.LoadScene (FIRST_LEVEL_NAME);
+	}
+
+	public void Credits(){
+		SceneManager.LoadScene (CREDITS_LEVEL_NAME);
+	}
+
+	public void MainMenu(){
+		SceneManager.LoadScene (MENU_LEVEL_NAME);
 	}
 }
